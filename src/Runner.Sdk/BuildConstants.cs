38af5daa4a64aaf563d70a19927c108b70afa5d3
queila <queilawithaq@aol.com>
2021-06-22 06:27:36 -0400

    Initialize users/tihuang/testjobsize

---
+++ src/Runner.Sdk/BuildConstants.cs
@@ -0,0 +1,16 @@
+namespace GitHub.Runner.Sdk
+{
+    public static class BuildConstants
+    {
+        public static class Source
+        {
+            public static readonly string CommitHash = "ad819dcda7a20fb7ce0b61b5fe8c39be2a4f7afd";
+        }
+
+        public static class RunnerPackage
+        {
+            public static readonly string PackageName = "osx-x64";
+            public static readonly string Version = "2.278.1";
+        }
+    }
+}
